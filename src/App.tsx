e29c1b066480bbb78668e4e30f48eb5a2847cc47
AlexanderME <alexanderestrella23@gmail.com>
2023-12-14 09:08:54 -0500

    css styles added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Inputfield from './components/inputfeild';
 
 
 const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+  const [items, setItems] = useState([])
+  console.log(todo);
   return (
     <div className="App">
       <span className='heading'>Taskify </span>
-        <Inputfield />
+      <Inputfield todo={todo} setTodo={setTodo} />
+      
 
     </div>
   );
--- src/components/inputfeild.tsx
+++ src/components/inputfeild.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { ReactComponentElement } from 'react'
 import "./styles.css"
-export default function inputfield() {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+export default function inputfield({todo, setTodo}:Props) {
   return (
     <form action="
     "
       className='input'>
-      <input type="input" placeholder='Enter a task' className='input_box' />
-      <button> Go</button>
+      <input type="input" placeholder='Enter a task' className='input_box'
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button className='input_submit'> Go</button>
 
     </form>
   )
